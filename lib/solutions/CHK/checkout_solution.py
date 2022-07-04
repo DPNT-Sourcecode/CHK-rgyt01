fff4dfa5242e871c6aaefa76b22541b6de2ec177
sbx_user1051 <sbx_user1051@169.254.5.229>
2022-07-04 20:10:00 +0000

    Mon Jul 04 20:10:00 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    A = skus[0]
+    B = skus[1]
+    C = skus[2]
+    D = skus[3]
+    A_SO = A//3
+    B_SO = B//2
+    A_SI = A%3
+    B_SI = B%2
+    Total = A_SO*130 + A_SI*50 + B_SO*45 + B_SI*30 + C*20 + D*15
+    return (Total)
+
+
